fecea35053382fa7346a717f73a865d9f5ff2aef
Hussain Ali <ali.415code@gmail.com>
2020-12-04 09:24:47 -0800

    completed Header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,38 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+// Add elemeents using createElement
+
+const div = document.createElement('div')
+const date = document.createElement('span')
+const h1 = document.createElement('h1')
+const temp = document.createElement('span')
+
+//add class to elements
+
+div.classList.add('header')
+date.classList.add('date')
+temp.classList.add('temp')
+
+// add text
+
+date.textContent = 'MARCH 28, 2020'
+h1.textContent = 'Lamda Times'
+temp.textContent = '98°'
+
+//create structure 
+
+const entryPoint = document.querySelector('.header-container')
+entryPoint.appendChild(div)
+div.appendChild(date)
+div.appendChild(h1)
+div.appendChild(temp)
+
+//return div
+
+return div
+}
+
+Header()
